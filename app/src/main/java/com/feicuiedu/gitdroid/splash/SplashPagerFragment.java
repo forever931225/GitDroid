3116ced1b7f84d1f47030889b60e9da1f3119241
liliang <forever931225@163.com>
2016-08-01 11:19:17 +0800

    Splash,页面搭建

--- app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/feicuiedu/gitdroid/splash/SplashPagerFragment.java
@@ -13,6 +13,7 @@ import com.feicuiedu.gitdroid.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.relex.circleindicator.CircleIndicator;
 
 /**
  * Created by Administrator on 16-7-29.
@@ -20,7 +21,9 @@ import butterknife.ButterKnife;
 public class SplashPagerFragment extends Fragment {
 
     @BindView(R.id.viewPager)ViewPager viewPager;
-
+    private SplashPagerAdapter adapter;
+    @BindView(R.id.indicator)
+    CircleIndicator indicator;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,8 +34,48 @@ public class SplashPagerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
         viewPager = (ViewPager) view.findViewById(R.id.viewPager);
-        viewPager.setAdapter(null);
+        adapter = new SplashPagerAdapter(getContext());
+        viewPager.setAdapter(adapter);
+        indicator.setViewPager(viewPager);
+        //添加viewpager监听（为了动画）
+        viewPager.addOnPageChangeListener(pageColorListener);
+        viewPager.addOnPageChangeListener(phoneViewListener);
     }
+
+    private ViewPager.OnPageChangeListener pageColorListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    //手机动画效果处理（平移、缩放、透明度变化）
+    private ViewPager.OnPageChangeListener phoneViewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
