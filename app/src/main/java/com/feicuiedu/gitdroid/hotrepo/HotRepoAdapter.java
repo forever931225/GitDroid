30295bdfbcd91ead8d7a17b74e30fd07fd3805b8
liliang <tv4094@163.com>
2016-08-02 15:51:17 +0800

    主界面抽屉效果

---
+++ app/src/main/java/com/feicuiedu/gitdroid/hotrepo/HotRepoAdapter.java
@@ -0,0 +1,7 @@
+package com.feicuiedu.gitdroid.hotrepo;
+
+/**
+ * Created by Administrator on 16-8-2.
+ */
+public class HotRepoAdapter {
+}
--- app/src/main/java/com/feicuiedu/gitdroid/splash/SplashActivity.java
+++ app/src/main/java/com/feicuiedu/gitdroid/splash/SplashActivity.java
@@ -3,13 +3,39 @@ package com.feicuiedu.gitdroid.splash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.feicuiedu.gitdroid.MainActivity;
 import com.feicuiedu.gitdroid.R;
+import com.feicuiedu.gitdroid.commons.ActivityUtils;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private ActivityUtils activityUtils;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
+        activityUtils = new ActivityUtils(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void login(){
+       /* activityUtils.startActivity(LoginActivity.class);*/
+        finish();
+    }
+
+    @OnClick(R.id.btnEnter)
+    public void enter(){
+        activityUtils.startActivity(MainActivity.class);
+        finish();
+    }
+
 }
